169fa3c7b8cbb7c462ca532b4fb4a7978084e9d0
Matheus Richard <matheusrichardt@gmail.com>
2019-05-05 00:20:15 -0300

    Adds Bullet

--- include/Bullet.h
+++ include/Bullet.h
@@ -2,22 +2,20 @@
 #define BULLET_H
 
 #include "Component.h"
-
 #include <string>
 
 class Bullet : public Component {
 private:
   int damage;
   float distanceLeft;
-  Vec2 speed;  
+  Vec2 speed;
 
 public:
   Bullet(GameObject &associated, float angle, float speed, int damage, float maxDistance, std::string sprite);
-  ~Bullet();
-  int GetDamage();
   void Update(float dt);
   void Render();
   bool Is(std::string type);
+  int GetDamage();
 };
 
 #endif
--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -4,19 +4,15 @@
 Bullet::Bullet(GameObject &associated, float angle, float speed, int damage,
                float maxDistance, std::string sprite) : Component(associated) {
   this->associated.AddComponent(new Sprite(associated, sprite));
-  this->speed = Vec2(Vec2::Cos(angle), Vec2::Sin(angle)) * speed;
+  this->speed = Vec2::GetSpeed(angle) * speed;
   this->damage = damage;
   this->distanceLeft = maxDistance;
 }
 
-Bullet::~Bullet() {
-}
-
 void Bullet::Update(float dt) {
   if (this->distanceLeft > 0) {
-    this->associated.box.x += this->speed.x;  
-    this->associated.box.y += this->speed.y;
-    this->distanceLeft -= Vec2().GetDistance(Vec2(this->speed.x*dt, this->speed.y*dt));
+    this->associated.box.UpdatePos(speed * dt);
+    this->distanceLeft -= Vec2().GetDistance(speed * dt);
   } else {
     this->associated.RequestDelete();
   }
