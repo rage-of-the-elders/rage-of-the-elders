7d1981576314911abfd7416e2005b38ee78decb5
Matheus Richard <matheusrichardt@gmail.com>
2019-05-13 14:36:44 -0300

    Adds debug rendering to Collider

--- src/Collider.cpp
+++ src/Collider.cpp
@@ -1,4 +1,12 @@
 #include "Collider.h"
+#include "MathHelper.h"
+
+#ifdef DEBUG
+#include "Camera.h"
+#include "Game.h"
+
+#include <SDL2/SDL.h> // Shouldn't it be INCLUDE_SDL?
+#endif // DEBUG
 
 Collider::Collider(GameObject &associated, Vec2 scale, Vec2 offset) : Component(associated) {
   this->scale = scale;
@@ -6,18 +14,33 @@ Collider::Collider(GameObject &associated, Vec2 scale, Vec2 offset) : Component(
 }
 
 void Collider::Update(float dt) {
-  // Depois, faça com que o centro 
-  // dela seja igual ao centro da box de associated, adicionado do atributo offset
-  // rotacionado pelo ângulo de associated.
-
   this->box.SetSize(this->associated.box.w * this->scale.x,
                     this->associated.box.h * this->scale.y);
-  this->box.SetCenterPos((this->associated.box.GetCenter() + this->offset).Rotate(this->associated.angleDeg)); // TODO: CHECK THIS
-
+  this->box.SetCenterPos(Vec2().Rotate(this->associated.angleDeg));
+  this->box.SetCenterPos(this->associated.box.GetCenter() + this->offset);
 }
 
 void Collider::Render() {
+#ifdef DEBUG
+  Vec2 center(this->box.GetCenter());
+  SDL_Point points[5];
 
+  Vec2 point = (Vec2(this->box.x, this->box.y) - center).Rotate(associated.angleDeg) + center - Camera::pos;
+  points[0] = {(int)point.x, (int)point.y};
+  points[4] = {(int)point.x, (int)point.y};
+
+  point = (Vec2(this->box.x + this->box.w, this->box.y) - center).Rotate(associated.angleDeg) + center - Camera::pos;
+  points[1] = {(int)point.x, (int)point.y};
+
+  point = (Vec2(this->box.x + this->box.w, this->box.y + this->box.h) - center).Rotate(associated.angleDeg) + center - Camera::pos;
+  points[2] = {(int)point.x, (int)point.y};
+
+  point = (Vec2(this->box.x, this->box.y + this->box.h) - center).Rotate(associated.angleDeg) + center - Camera::pos;
+  points[3] = {(int)point.x, (int)point.y};
+
+  SDL_SetRenderDrawColor(Game::GetInstance().GetRenderer(), 255, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderDrawLines(Game::GetInstance().GetRenderer(), points, 5);
+#endif // DEBUG
 }
 
 bool Collider::Is(std::string type) {
