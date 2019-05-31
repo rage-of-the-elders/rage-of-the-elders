d7ec848f037d626a0ee1e2039a63f791bd9fb02c
Matheus Richard <matheusrichardt@gmail.com>
2019-05-31 11:51:46 -0300

    Adds Flicker on Camera

--- include/Camera.h
+++ include/Camera.h
@@ -3,10 +3,24 @@
 
 #include "GameObject.h"
 #include "Vec2.h"
+#include "Sprite.h"
+#include "Timer.h"
+#include "CameraFollower.h"
 
 class Camera {
 private:
   static GameObject *focus;
+  static Sprite *black;
+  static bool isBlack;
+  static bool isFlickering;
+  static float flickerDuration;
+  static float flickerFrequency;
+  static Timer flickerTimer;
+  static Timer flickerFrequencyTimer;
+  static CameraFollower* blackFollower;
+
+  static void AddBlack();
+  static void HandleFlicker(float dt);
 
 public:
   static Vec2 pos;
@@ -16,6 +30,8 @@ public:
   static void Unfollow();
   static void Update(float dt);
   static GameObject *GetFocus();
+  static void RenderBlack();
+  static void Flicker(float duration, float frequency = 0.5);
 };
 
 #endif
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -5,6 +5,14 @@
 GameObject *Camera::focus = nullptr;
 Vec2 Camera::pos = Vec2();
 Vec2 Camera::speed = Vec2(100, 100);
+bool Camera::isBlack = false;
+bool Camera::isFlickering = false;
+float Camera::flickerDuration = 0;
+float Camera::flickerFrequency = 0;
+Timer Camera::flickerTimer = Timer();
+Timer Camera::flickerFrequencyTimer = Timer();
+Sprite *Camera::black = nullptr;
+CameraFollower *Camera::blackFollower = nullptr;
 
 void Camera::Follow(GameObject *newFocus) {
   focus = newFocus;
@@ -30,8 +38,57 @@ void Camera::Update(float dt) {
 		if(InputManager::GetInstance().IsKeyDown(RIGHT_ARROW_KEY))
 			pos.x += speed.x*dt;
   }
+
+  HandleFlicker(dt);
 }
 
 GameObject *Camera::GetFocus() {
   return focus;
+}
+
+void Camera::AddBlack() {
+  GameObject *blackGO = new GameObject();
+  black = new Sprite(*blackGO, "img/black.png");
+  blackGO->AddComponent(black);
+  blackFollower = new CameraFollower(*blackGO);
+  black->Desactivate();
+}
+
+void Camera::RenderBlack() {
+  if (black == nullptr)
+      AddBlack();
+
+  if (black->IsActive()) {
+    black->Render();
+  }
+}
+
+void Camera::Flicker(float duration, float frequency) {
+  if (not isFlickering) {
+    flickerTimer = Timer();
+    flickerFrequencyTimer = Timer();
+    flickerDuration = duration;
+    flickerFrequency = frequency;
+    isFlickering = true;
+  }
+}
+
+void Camera::HandleFlicker(float dt) {
+  if (isFlickering) {
+    flickerTimer.Update(dt);
+    flickerFrequencyTimer.Update(dt);
+    blackFollower->Update(dt);
+
+    if (flickerTimer.Get() < flickerDuration) {
+      if (flickerFrequencyTimer.Get() < flickerFrequency) {
+        black->Activate();
+      } else {
+        black->Desactivate();
+        flickerFrequencyTimer = Timer();
+      }
+    } else {
+      black->Desactivate();
+      isFlickering = false;
+    }
+  }
 }
